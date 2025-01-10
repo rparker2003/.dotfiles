168c24fd5b312cb6761dd58c79b18998ac041684
RyanParker <rparker52003@gmail.com>
2025-01-10 10:51:07 -0800

    Fixed copilot accept hotkey conflicts
    blink.cmp was overriding the <C-y> command (ctrl+y)
    so I just disabled it as I don't believe I use it.

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -18,6 +18,7 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { "blink.cmp", enabled = false },
     -- import/override with your plugins
     { import = "plugins" },
   },
