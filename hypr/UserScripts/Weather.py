9aafd45f39efef6266b689e377747d05d30bbfba
Ryan Parker <rparker52003@gmail.com>
2025-02-24 20:05:30 -0800

    Initial commit of arch-pc custom config for hyprland and zsh

--- hypr/UserScripts/Weather.py
+++ hypr/UserScripts/Weather.py
@@ -26,7 +26,7 @@ weather_icons = {
 # to get your own location_id, go to https://weather.com & search your location.
 # once you choose your location, you can see the location_id in the URL(64 chars long hex string)
 # like this: https://weather.com/en-PH/weather/today/l/bca47d1099e762a012b9a139c36f30a0b1e647f69c0c4ac28b537e7ae9c1c200
-location_id = "bca47d1099e762a012b9a139c36f30a0b1e647f69c0c4ac28b537e7ae9c1c200"  # TODO
+location_id = "792536cbcb7b658a86e0c2d0f6a4d0754a189752fd746af15db10c3af0894cb2"  # TODO
 
 # NOTE to change to deg F, change the URL to your preffered location after weather.com
 # Default is English-Philippines with Busan, South Korea as location_id
